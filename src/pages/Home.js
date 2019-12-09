51d05598ee5dfb33e66319f4ed8217b710a8a75c
dbills777 <dbills777@gmail.com>
2019-12-09 07:08:36 -0700

    adding repo

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import Services from "../components/Services";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Hero>
+        <Banner title="Luxurious Rooms" subtitle="Delux Rooms Starting At #299">
+          <Link to="/rooms" className="btn-primary">
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </React.Fragment>
+  );
+}
