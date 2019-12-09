863b363916a14d29ce3da4256c050a684d14396f
dbills777 <dbills777@gmail.com>
2019-12-09 07:47:52 -0700

    added services state

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,52 @@
 import React, { Component } from "react";
 import Title from "./Title";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 
 export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "free cocktails",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae iste commodi possimus reprehenderit doloribus quaerat eligendi delectus quod debitis."
+      },
+      {
+        icon: <FaHiking />,
+        title: "Hiking Trails",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae iste commodi possimus reprehenderit doloribus quaerat eligendi delectus quod debitis."
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "free shuttles",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae iste commodi possimus reprehenderit doloribus quaerat eligendi delectus quod debitis."
+      },
+      {
+        icon: <FaBeer />,
+        title: "great beers on tap",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae iste commodi possimus reprehenderit doloribus quaerat eligendi delectus quod debitis."
+      }
+    ]
+  };
   render() {
     return (
-      <div>
+      <section className="services">
         <Title title="services" />
-      </div>
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
     );
   }
 }
